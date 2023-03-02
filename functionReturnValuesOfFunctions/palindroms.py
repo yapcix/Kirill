c1eeea52968fbfde25be49ec6a02ac2ff7432cbe
r <t>
2023-03-02 19:42:38 +0300

    return

---
+++ functionReturnValuesOfFunctions/palindroms.py
@@ -0,0 +1,24 @@
+def palindrome():
+    s = 'А роза упала на лапу Азора'
+    s = s.lower().replace(' ', '')
+    # r = reversed(s)
+
+    return s
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    print(palindrome())
+
+if __name__ == '__main__':
+    main()
